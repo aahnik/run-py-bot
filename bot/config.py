fb72651fb9a1d1a4959eb71e726850d95c812735
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-11-06 11:54:47 +0530

    unban ** and set timeout message

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,7 @@
 # expressions which are banned
-banned =  ['quit', 'input', 'open', 'import', '**','exit']
+banned =  ['quit', 'input', 'open', 'import','exit']
 
 # timeout in seconds
 timeout =  6
+
+timeout_message = '😢 Timeout reached. I have limited resources. \nYou may increase the timeout and run this bot on your own server if required.'
