16f660d7ac4e53ff081b17e6713db4129400c5bf
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-11-06 11:56:22 +0530

    add new help, code info and handle long messages

--- bot/runPython_bot.py
+++ bot/runPython_bot.py
@@ -1,25 +1,6 @@
 '''
-I'm the *Python Runner* bot 😀 
-
-I have been created by @AahnikDaw and you may see my [source code](https://github.com/aahnik/run-py-bot) on GitHub.
-
-I am currently *alive and kicking* on a free tier server of Python Anywhere.
-
-😁 Sometimes I behave quite differently from a regular python interactive shell. 
-Certain things are not allowed to prevent horrible consequences.
-
-_Start using me to figure out more about me_.
-
-Every message is executed freshly. Suppose you define a variable in one message, you wont be able to access it in the next message.
-
-But you can write _multiline code in one message_
-->Tab is 4 spaces
--> Newline is a newline in the same message body
-
-If I am being used by too many people, there may be delay in response as I am deployed on a free tier server.
+This module makes the bot actually run
 '''
-
-# the module docstring will be sent to the user if the bot is alive
 
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 # python-telegram-bot is a Pythonic Wrapper to the core Telegram API
@@ -28,11 +9,27 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 # learn more about it here https://github.com/python-telegram-bot/python-telegram-bot
 
 
-from .execute_code import run
+from .execute_code import eval_py, run
 
 # read the token for authenticating our bot
 with open('token.txt') as f:
     tok = f.readline().strip()
+
+with open('info/start.txt') as f:
+    start_text = f.read()
+
+with open('info/help.txt') as f:
+    help_text = f.read()
+
+with open('info/code.txt') as f:
+    code_text = f.read()
+
+
+def handle_long_message(msg):
+    if len(msg) > 2000:
+        return msg[:2000]+'\n\n 😟 Output was too long, truncated to 2000 characters'
+    else:
+        return msg
 
 
 def bot():
@@ -52,25 +49,50 @@ def bot():
         '''
 
         context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f'{__doc__}\n', parse_mode='Markdown')
+            chat_id=update.effective_chat.id, text=start_text, parse_mode='Markdown')
         # for more info on parse modes see https://python-telegram-bot.readthedocs.io/en/stable/telegram.parsemode.html
 
-    def reply(update, context):
+    def help(update, context):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text=help_text, parse_mode='Markdown')
+
+    def code_info(update, context):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text=code_text, parse_mode='Markdown')
+
+    def reply_execute(update, context):
         '''
         This function replies to any non-command messages
         '''
+
         returned_val = run(update)
-        if len(returned_val) > 2000:
-            returned_val = returned_val[:2000]
-            returned_val += '\n\n 😟 Output was too long, truncated to 2000 characters'
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text=returned_val)
+        message = handle_long_message(returned_val)
+        update.message.reply_text(message, quote=True)
 
-    start_handler = CommandHandler('start', start)
-    message_handler = MessageHandler(Filters.text & (~Filters.command), reply)
+    def reply_eval(update, context):
+        if context.args:
+            input_text = ''
+            for string in context.args:
+                input_text += string + ' '
+            out = eval_py(input_text)
+            message = handle_long_message(out)
+            update.message.reply_text(message, quote=True)
+        else:
+            update.message.reply_text(
+                'No expression provided to eval. \nUsage Example: \n/e `4 >= 5`', quote=True, parse_mode='Markdown')
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(message_handler)
+    _handlers = {}
+
+    _handlers['start_handler'] = CommandHandler('start', start)
+    _handlers['help_handler'] = CommandHandler('help', help)
+    _handlers['code_info_handler'] = CommandHandler('code', code_info)
+    _handlers['message_handler'] = MessageHandler(
+        Filters.text & (~Filters.command), reply_execute)
+    _handlers['eval_handler'] = CommandHandler('e', reply_eval)
+
+    for name, _handler in _handlers.items():
+        print(f'Adding {name}')
+        dispatcher.add_handler(_handler)
 
     updater.start_polling()
     updater.idle()
