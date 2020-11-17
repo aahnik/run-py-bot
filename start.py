d796786859e7ec8ba0b23cb186c6a184aa1c9b09
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-17 17:06:43 +0530

    update start.py

--- start.py
+++ start.py
@@ -12,6 +12,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO, datefmt='%d/%m/%Y %I:%M:%S %p')
 
 logger = logging.getLogger()
-logger.setLevel(20)
+logger.setLevel(logging.INFO)
 
 bot.main()
