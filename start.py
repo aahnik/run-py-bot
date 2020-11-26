0386ad49ac3077388ec6c2386ed8dbad2b7617e0
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-26 20:15:11 +0530

    remove quart

--- start.py
+++ start.py
@@ -1,25 +1,10 @@
 ''' Script to start the bot '''
+
 import logging
-from quart import Quart
 
-
-from run_py_bot import main, config
+from run_py_bot import main
 
 logging.basicConfig(level=logging.INFO)
 
-app = Quart(__name__)
-
-
-@app.route('/')
-async def hello():
-    return 'hello'
-
-
-@app.route(f'/{config.API_TOKEN}')
-async def info():
-    info = await main.info()
-    return str(info)
-
 if __name__ == "__main__":
     main.webhook()
-    # app.run(host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
