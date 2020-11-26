74b17bd2df95612f879baf82d5a77d5961371e0b
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-26 19:35:27 +0530

    await

--- sethook.py
+++ sethook.py
@@ -8,6 +8,6 @@ bot = Bot(token=config.API_TOKEN)
 async def hook_and_crook():
     await bot.delete_webhook()
     await bot.set_webhook(config.WEBHOOK_URL)
-    print(bot.get_webhook_info())
+    print(await bot.get_webhook_info())
 
 asyncio.run(hook_and_crook())
