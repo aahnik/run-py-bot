f7f384b272f18b913b7eb8587e6ecf6341d2d8cf
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-26 17:12:30 +0530

    delete the junk

--- start.py
+++ start.py
@@ -1,29 +1,10 @@
-''' This module will setup logging in desired timezone and start the bot. '''
+''' Script to start the bot '''
 
 import logging
-from datetime import datetime
-from pytz import timezone
+
 from bot import bot
-from bot.config import DOMAIN,PROJECT_SUBDOMAIN, METHOD
 
-
-TIMEZONE = 'Asia/Kolkata'
-
-logging.Formatter.converter = lambda *args: datetime.now(
-    tz=timezone(TIMEZONE)).timetuple()
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO, datefmt='%d/%m/%Y %I:%M:%S %p')
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    if METHOD == 'webhook':
-        assert DOMAIN
-        assert PROJECT_SUBDOMAIN
-        bot.webhook()
-    elif METHOD == 'polling':
-        bot.poll()
-    else:
-        print('This should never happen.')
+    bot.webhook()
