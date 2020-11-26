dc6c09fc410d76e326982aac0c6daed118ef963d
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-26 20:02:49 +0530

    experiment

--- start.py
+++ start.py
@@ -21,5 +21,5 @@ async def info():
     return str(info)
 
 if __name__ == "__main__":
-    app.run(host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
     main.webhook()
+    # app.run(host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
