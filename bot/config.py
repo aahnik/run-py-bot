701b6da67729d940d9bb2c51a486f58cdc9764f8
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-26 19:11:10 +0530

    update

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,4 @@
 import os
-from secrets import token_urlsafe
 
 with open('messages/start.txt') as f:
     START_MESSAGE = f.read()
@@ -17,11 +16,10 @@ assert API_TOKEN
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 assert HEROKU_APP_NAME
 
-webhook_secret = token_urlsafe(32)
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{webhook_secret}'
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
