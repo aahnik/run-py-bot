a0085df4254b60280900a48a55a779f047527bf5
aahnik <daw@aahnik.dev>
2021-04-20 09:50:18 +0530

    ✨ provide both start options poll or webhook

--- start.py
+++ start.py
@@ -4,7 +4,12 @@ import logging
 
 from run_py_bot import main
 
+from run_py_bot.config import HEROKU_APP_NAME
+
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    main.webhook()
+    if HEROKU_APP_NAME:
+        main.webhook()
+    else:
+        main.poll()
